df0b6bc19c9203ffcf4498d3bcc993f1ac6d64a4
Andrés Morales Reyes <sharker92@hotmail.com>
2024-07-01 12:02:32 -0600

    📦 NEW: 2.9

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesController } from './coffees/coffees.controller';
-import { ArstController } from './arst/arst.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, CoffeesController, ArstController],
+  controllers: [AppController, CoffeesController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -9,5 +9,9 @@ export class CoffeesController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return `This action returns #[${id}] coffee`;
+  }
+  @Post()
+  create(@Body() body) {
+    return body;
   }
 }
