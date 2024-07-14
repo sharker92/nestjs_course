fec6f1267def396cdf336173cc91874b89a273d4
Andrés Morales Reyes <sharker92@hotmail.com>
2024-07-14 17:03:06 -0600

    📦 NEW: 2.14

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -8,33 +8,34 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
-import { query } from 'express';
+import { CoffeesService } from './coffees.service';
 
 @Controller('coffees')
 export class CoffeesController {
+  constructor(private readonly coffeeService: CoffeesService) {}
   @Get()
   findAll(@Query() paginationQuery) {
-    const { limit, offset } = paginationQuery;
-    return `This action returns all coffees. Limit: ${limit}, ${offset}`;
+    // const { limit, offset } = paginationQuery;
+    return this.coffeeService.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return `This action returns #[${id}] coffee`;
+    return this.coffeeService.findOne(id);
   }
 
   @Post()
   create(@Body() body) {
-    return body;
+    return this.coffeeService.create(body);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() body) {
-    return `This action updates #${id} coffee`;
+    return this.coffeeService.update(id, body);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return `This action removes #${id} coffee`;
+    return this.coffeeService.remove(id);
   }
 }
