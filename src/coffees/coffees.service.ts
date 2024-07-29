0532e5a8f3580d6e9b84543baa9c87346485da37
Andrés Morales Reyes <sharker92@hotmail.com>
2024-07-29 10:06:19 -0600

    📦 NEW: 3.27

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -18,10 +18,13 @@ export class CoffeesService {
   ) {}
 
   findAll() {
-    return this.coffeeRepository.find();
+    return this.coffeeRepository.find({ relations: ['flavors'] });
   }
   async findOne(id: number) {
-    const coffee = await this.coffeeRepository.findOne({ where: { id } });
+    const coffee = await this.coffeeRepository.findOne({
+      where: { id },
+      relations: ['flavors'],
+    });
     if (!coffee) {
       throw new NotFoundException(`Coffee #${id} not found`);
     }
