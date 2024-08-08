0249c6c38568b7005c6ae0a7bf51fb7f8d14d45a
Andrés Morales Reyes <sharker92@hotmail.com>
2024-08-08 10:17:21 -0600

    👌 IMPROVE: not sure what's going on with git...

---
+++ 1722968571392-SchemaSync.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class SchemaSync1722968571392 implements MigrationInterface {
+    name = 'SchemaSync1722968571392'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "coffee" ADD "description" character varying`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "coffee" DROP COLUMN "description"`);
+    }
+
+}
