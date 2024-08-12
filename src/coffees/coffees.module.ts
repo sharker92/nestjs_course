72bc84a32ec164e51e3774adc5768111537fc34b
Andrés Morales Reyes <sharker92@hotmail.com>
2024-08-12 13:03:24 -0600

    📦 NEW: 3.34-2

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -4,12 +4,12 @@ import { CoffeesService } from './coffees.service';
 import { Coffee } from './entities/coffee.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Flavor } from './entities/flavor.entity';
-// import { Event } from '../events/entities/event.entity';
+import { Event } from '../events/entities/event.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
   providers: [CoffeesService],
-  // exports: [CoffeesService],
+  exports: [CoffeesService],
 })
 export class CoffeesModule {}
