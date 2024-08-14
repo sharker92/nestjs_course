31bc9363affcacafbad2ca7d9ae9286c84df0a45
Andrés Morales Reyes <sharker92@hotmail.com>
2024-08-14 13:33:13 -0600

    📦 NEW: 3.38

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -7,6 +7,9 @@ import { Flavor } from './entities/flavor.entity';
 import { Event } from '../events/entities/event.entity';
 import { COFFEE_BRANDS } from './coffees.constants';
 
+class ConfigService {}
+class DeveloptmentConfigService {}
+class ProductionConfigService {}
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
