8b996e8f9a1039181eeaabd8386cc3863d93c276
Andrés Morales Reyes <sharker92@hotmail.com>
2024-08-14 13:45:23 -0600

    📦 NEW: 3.39

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Injectable, Module } from '@nestjs/common';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
 import { Coffee } from './entities/coffee.entity';
@@ -10,12 +10,33 @@ import { COFFEE_BRANDS } from './coffees.constants';
 class ConfigService {}
 class DeveloptmentConfigService {}
 class ProductionConfigService {}
+
+@Injectable()
+export class CoffeeBrandsFactory {
+  create() {
+    /* ... do something */
+    return ['buddy brew', 'nescafe'];
+  }
+}
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
   providers: [
     CoffeesService,
-    { provide: COFFEE_BRANDS, useValue: ['buddy brew', 'nescafe'] },
+    CoffeeBrandsFactory,
+    {
+      provide: ConfigService,
+      useClass:
+        process.env.NODE_ENV === 'development'
+          ? DeveloptmentConfigService
+          : ProductionConfigService,
+    },
+    {
+      provide: COFFEE_BRANDS,
+      useFactory: (brandsFactory: CoffeeBrandsFactory) =>
+        brandsFactory.create(),
+      inject: [CoffeeBrandsFactory],
+    },
   ],
   exports: [CoffeesService],
 })
