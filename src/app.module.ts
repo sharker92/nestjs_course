f779952a1733db429ff40d212245504772f835af
Andrés Morales Reyes <sharker92@hotmail.com>
2024-08-22 11:20:29 -0600

    📦 NEW: 4.46

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,10 +7,15 @@ import { dataSourceOptions } from 'data-source';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        DATABASE_HOST: Joi.required(),
+        DATABASE_PORT: Joi.number().default(5434),
+    }),}),
     CoffeesModule,
     TypeOrmModule.forRoot({
       ...dataSourceOptions,
