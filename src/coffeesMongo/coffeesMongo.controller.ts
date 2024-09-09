c65d3ae27e7fb513b672ff0972d71a01f82e21b3
Andrés Morales Reyes <sharker92@hotmail.com>
2024-09-09 11:16:37 -0600

    📦 NEW: 7.77

--- src/coffeesMongo/coffeesMongo.controller.ts
+++ src/coffeesMongo/coffeesMongo.controller.ts
@@ -7,57 +7,40 @@ import {
   Patch,
   Post,
   Query,
-  UsePipes,
 } from '@nestjs/common';
 
 import { CoffeesMongoService } from './coffeesMongo.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
-import { Public } from 'src/common/decorators/public.decorator';
-import { Protocol } from 'src/common/decorators/protocol.decorator';
-import { ParseIntPipe2 } from 'src/common/pipes/parse-int/parse-int2.pipe';
 import { ParseIntPipe } from 'src/common/pipes/parse-int/parse-int.pipe';
-import { ApiForbiddenResponse, ApiResponse, ApiTags } from '@nestjs/swagger';
 
-@ApiTags('coffeesMongo')
-@Controller('coffees')
+@Controller('coffeesMongo')
 export class CoffeesMongoController {
   constructor(private readonly coffeeService: CoffeesMongoService) {
     console.log('CoffeesController instantiated');
   }
-  @ApiResponse({ status: 403, description: 'Forbidden.' })
-  @Public()
   @Get()
-  async findAll(
-    @Protocol('hello') protocol: string,
-    @Query() paginationQuery: PaginationQueryDto,
-  ) {
-    console.log(protocol);
-    await new Promise((resolve) => setTimeout(resolve, 2000));
+  async findAll(@Query() paginationQuery: PaginationQueryDto) {
     return this.coffeeService.findAll(paginationQuery);
   }
-  @UsePipes(ParseIntPipe2)
   @Get(':id')
-  findOne(@Param('id', ParseIntPipe) id: number) {
+  findOne(@Param('id') id: string) {
     console.log(id);
     return this.coffeeService.findOne(id);
   }
-  @ApiForbiddenResponse({ description: 'Forbidden.' })
   @Post()
   create(@Body() createCoffeeDto: CreateCoffeeDto) {
     console.log(createCoffeeDto);
     return this.coffeeService.create(createCoffeeDto);
   }
 
-  @ApiTags('coffees1', 'coffees2')
   @Patch(':id')
-  update(@Param('id') id: number, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+  update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
     return this.coffeeService.update(id, updateCoffeeDto);
   }
-  @ApiTags('coffees1')
   @Delete(':id')
-  remove(@Param('id') id: number) {
+  remove(@Param('id') id: string) {
     return this.coffeeService.remove(id);
   }
 }
--- src/coffeesMongo/coffeesMongo.service.ts
+++ src/coffeesMongo/coffeesMongo.service.ts
@@ -1,16 +1,45 @@
-import { Injectable, Scope } from '@nestjs/common';
+import { Injectable, NotFoundException, Scope } from '@nestjs/common';
 import { Coffee } from './entities/coffee.entity';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 @Injectable({ scope: Scope.DEFAULT })
 export class CoffeesMongoService {
-  constructor() {}
+  constructor(
+    @InjectModel(Coffee.name) private readonly coffeeModel: Model<Coffee>,
+  ) {}
 
-  findAll(paginationQuery: PaginationQueryDto) {}
-  async findOne(id: number) {}
-  async create(createCoffeeDto: CreateCoffeeDto) {}
-  async update(id: number, updateCoffeeDto: UpdateCoffeeDto) {}
-  async remove(id: number) {}
+  findAll(paginationQuery: PaginationQueryDto) {
+    return this.coffeeModel.find().exec();
+  }
+  async findOne(id: string) {
+    const coffee = await this.coffeeModel.findOne({ _id: id }).exec();
+    if (!coffee) {
+      throw new NotFoundException(`Coffee #${id} not found`);
+    }
+    return coffee;
+  }
+  async create(createCoffeeDto: CreateCoffeeDto) {
+    const coffee = new this.coffeeModel(createCoffeeDto);
+    return coffee.save();
+  }
+  async update(id: string, updateCoffeeDto: UpdateCoffeeDto) {
+    const existingCoffee = await this.coffeeModel
+      .findOneAndUpdate({ _id: id }, { $set: updateCoffeeDto }, { new: true })
+      .exec();
+
+    if (!existingCoffee) {
+      throw new NotFoundException(`Coffee #${id} not found`);
+    }
+    return existingCoffee;
+  }
+  async remove(id: string) {
+    const result = await this.coffeeModel.deleteOne({ _id: id }).exec();
+    if (result.deletedCount === 0) {
+      throw new NotFoundException(`Coffee #${id} not found`);
+    }
+  }
   async recommendCoffee(coffee: Coffee) {}
 }
