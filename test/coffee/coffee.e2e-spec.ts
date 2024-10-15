7cc4bbf25410465547c47d3060d5785af9165a93
Andrés Morales Reyes <sharker92@hotmail.com>
2024-10-15 10:59:33 -0600

    👌 IMPROVE: Update Jasmine2 to jest-circus.

--- test/coffee/coffee.e2e-spec.ts
+++ test/coffee/coffee.e2e-spec.ts
@@ -1,12 +1,12 @@
 import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { CoffeesModule } from 'src/coffees/coffees.module';
-import { CreateCoffeeDto } from 'src/coffees/dto/create-coffee.dto';
 import * as request from 'supertest';
+import { CoffeesModule } from '../../src/coffees/coffees.module';
+import { CreateCoffeeDto } from '../../src/coffees/dto/create-coffee.dto';
 
 // we can mock interactions database, use a disk-based database, or use a real database
-describe('', async () => {
+describe('[Feature] Coffees - /coffees', () => {
   const coffee = {
     name: 'Shipwreck Roast',
     brand: 'Buddy Brew',
@@ -49,16 +49,31 @@ describe('', async () => {
       .send(coffee as CreateCoffeeDto)
       .expect(HttpStatus.CREATED) // until here could be enough
       .then(({ body }) => {
-        const expectedCoffee = jasmine.objectContaining({
+        const expectedCoffee = expect.objectContaining({
           ...coffee,
-          flavors: jasmine.arrayContaining(
-            coffee.flavors.map((name) => jasmine.objectContaining({ name })), // TODO revisar las salida del body para entender esta logica
+          flavors: expect.arrayContaining(
+            coffee.flavors.map((name) => expect.objectContaining({ name })), // TODO revisar las salida del body para entender esta logica
           ),
         }); // optionally could create files with expected responses and DTO to avoid this repeating logic
         expect(body).toEqual(expectedCoffee);
       });
   });
 
+  it('Create [POST /coffees]2', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/coffees')
+      .send(coffee as CreateCoffeeDto)
+      .expect(HttpStatus.CREATED);
+
+    const expectedCoffee = expect.objectContaining({
+      ...coffee,
+      flavors: expect.arrayContaining(
+        coffee.flavors.map((name) => expect.objectContaining({ name })),
+      ),
+    });
+
+    expect(response.body).toEqual(expectedCoffee);
+  });
   it.todo('Get all [GET /coffees]');
   it.todo('Get one [GET /coffees/:id]');
   it.todo('Update [PATCH /coffees/:id]');
@@ -68,3 +83,4 @@ describe('', async () => {
     await app.close();
   });
 });
+//todo this file is working?
